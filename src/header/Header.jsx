84e6f279093252f094da01e07b1049d5cc6e4427
jaidan2006 <jaidanray@gmail.com>
2026-01-06 23:39:41 +0530

    first commit

---
+++ src/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import "./Header.css"
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+const Header = () => {
+  return (
+   <div className="topbar">
+      <div className="topbar-left">
+        <span>
+          <i className="fas fa-phone"></i><a href="tel:+914466949352">044-66949352</a>
+
+        </span>
+        <span>
+          <i className="fas fa-envelope"></i> <a href="https://mail.google.com/mail/?view=cm&fs=1&to=info@valleypointtechnologies.com">info@valleypointtechnologies.com</a>
+
+        </span>
+      </div>
+
+      <div className="topbar-right">
+        <a href="https://www.facebook.com/ValleyPointTechnologies/photos/"><i className="fab fa-facebook-f"></i></a>
+        <a href="https://x.com/valleypointtech"><i className="fab fa-twitter"></i></a>
+        <a href="https://www.youtube.com"><i className="fab fa-youtube"></i></a>
+        <a href="https://www.instagram.com/valleypointtechnologies/"><i className="fab fa-instagram"></i></a>
+        <a href="https://in.linkedin.com/company/valley-point-technologies"><i className="fab fa-linkedin-in"></i></a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
